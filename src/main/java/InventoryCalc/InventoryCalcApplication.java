a472c41a6e3de96b2767051e949d995960594c04
Heather <>
2023-09-25 00:50:07 -0500

    first commit

---
+++ src/main/java/InventoryCalc/InventoryCalcApplication.java
@@ -0,0 +1,13 @@
+package InventoryCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/InventoryCalc/InventoryCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package InventoryCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
